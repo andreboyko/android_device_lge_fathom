10fff0a5394889a3a0986afa5c596a790b3e2afa
Andrew Boyko <www2287@yandex.ru>
2012-02-08 15:24:27 +0400

    BoardConfig optimisations

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,42 +3,40 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/lge/fathom/BoardConfigVendor.mk
 
-#building options
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RECOVERY := true
 TARGET_NO_RADIOIMAGE := true
 
+TARGET_NO_BOOTLOADER := true
 
-#chipset features
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_BOARD_PLATFORM := qsd8k
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+#Do we need it?
 TARGET_BOOTLOADER_BOARD_NAME := fathom
 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
 #kernel
-BOARD_KERNEL_CMDLINE := rel_path=2.2 data_size=256 cache_size=60
+BOARD_KERNEL_CMDLINE := androidboot.hardware=fathom data_size=256 cache_size=60
 BOARD_KERNEL_BASE := 0x11800000
 BOARD_KERNEL_NEW_PPPOX := true
+
+TARGET_PREBUILT_KERNEL := device/lge/fathom/kernel
 
 #jit
 WITH_JIT := true
 ENABLE_JSC_JIT := true
 JS_ENGINE := v8
-
-
-# fix this up by examining /proc/mtd on a running device
-#BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00500000
-#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
-#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x9600000
-#BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x10000000
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-
-TARGET_PREBUILT_KERNEL := device/lge/fathom/kernel
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
@@ -67,8 +65,8 @@ BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 #sensors
 
 #fm - not working now
-BOARD_HAVE_FM_RADIO := true
-BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+#BOARD_HAVE_FM_RADIO := true
+#BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 
 #bluetooth
 BOARD_HAVE_BLUETOOTH := true
